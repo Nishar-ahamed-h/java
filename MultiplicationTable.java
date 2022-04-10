1cf7bf23b0455b39bab924c7a3b6f67c2abc5f9e
Nishar Ahamed H <91793825+Nishar-ahamed-h@users.noreply.github.com>
2022-04-10 22:45:12 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,13 @@
+package example;
+import java.util.*;
+public class MultiplicationTable {
+	public static void main(String[] args) {	
+		        int num = 3;
+		        for(int i = 1; i <= 15; ++i)
+		        {
+		            System.out.printf("%d * %d = %d \n", num, i, num * i);
+		        }
+		    }
+		}
+
+

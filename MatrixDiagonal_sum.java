79cfd81ba6a1ccdf3efddeedcfbec4689cc002c7
Nishar Ahamed H <91793825+Nishar-ahamed-h@users.noreply.github.com>
2022-04-12 21:25:01 +0530

    Update MatrixDiagonal_sum.java

--- MatrixDiagonal_sum.java
+++ MatrixDiagonal_sum.java
@@ -17,6 +17,7 @@ public class MatrixDiagonal_sum  {
       int sum=0;
       for(int i=0;i<r;i++) {
     	  for(int j=0;j<c;j++) {
+		  if(i==j)
     		 sum= sum + a[i][j];
     	  }}
     		 System.out.println("Sum of Diagonals of the element=" +sum); 
